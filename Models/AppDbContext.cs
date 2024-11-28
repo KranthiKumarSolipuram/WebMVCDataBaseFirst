d64be0ed8842dac02f65351ce63cc96edf901a31
kranthikumar.solipur <kranthikumar.solipur@SNAD-34>
2024-11-28 16:51:26 +0530

    Add project files.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebMVCDataBaseFirst.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ToDo> ToDos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=testdb;User ID=sduser;Password=12345;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/ToDo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebMVCDataBaseFirst.Models;
+
+public partial class ToDo
+{
+    public int Id { get; set; }
+
+    public string Task { get; set; } = null!;
+
+    public bool IsCompleted { get; set; }
+}
